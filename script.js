42151e690f7d7d5b6a0989ddae28060b0960e50d
vamsi-work <forw8568@gmail.com>
2024-09-27 07:16:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#heroCarousel').carousel({
+        interval: 3000 // Change slide every 5 seconds
+    });
+});
+
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
